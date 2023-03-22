500362dafa8f0ecfac493f5cce0f105a547c0341
harshwardhanth <123435753+harshwardhanth@users.noreply.github.com>
2023-03-22 10:17:30 +0530

    Update tik tac.c

--- tik tac.c
+++ tik tac.c
@@ -120,6 +120,29 @@ int checkwin()
 FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
  ********************************************************************/
 
+void board()
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+
+    printf("     |     |     \n\n");
+}
 
 
 /*******************************************************************
